0fbb76e7802f2c71b7a2fa0db4b252d34cb4db01
Mike Dupree <mikerdupree@gmail.com>
2023-02-13 07:26:03 -0800

    Fix build issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-})
-module.exports = withBundleAnalyzer({})
+  swcMinify: false,
+}
